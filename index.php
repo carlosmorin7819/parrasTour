26b31ef580092b5e3754a695dd60dd4100103df2
carlosmorin7819 <carlosmorin7819@hotmail.com>
2017-09-26 01:21:43 -0500

    inicio de proyecto

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>PARRAS</title>
+	<link href="https://fonts.googleapis.com/css?family=Arsenal" rel="stylesheet">
+	<link href="CSS/icons/css/font-awesome.css" rel="stylesheet">
+	<link rel="stylesheet" href="CSS/helpers.css">
+	<link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+	<header>
+		
+	</header>
+	<body>
+		<div class="container">
+			<ul class="list row">
+				<li class="wathDo col-xs-6  bgBlue h120 block noPadding ">
+					<div class="content middle text-center">
+						<i class="fa fa-list-ul s40 cWhite text-shadow" aria-hidden="true"></i><br>
+						<span class="cWhite text-shadow s18 text-600">Que hacer?</span>	
+					</div>
+				</li>
+				<li class="places col-xs-6  bgBlue h120 block noPadding ">
+					<div class="content middle text-center">
+						<i class="fa fa-map-marker s40 cWhite text-shadow" aria-hidden="true"></i><br>
+						<span class="cWhite text-shadow s18 text-600">Lugares</span>	
+					</div>
+				</li>
+				<li class="wathDo col-xs-6  bgBlue h120 block noPadding ">
+					<div class="content middle text-center">
+						<i class="fa fa-list-ul s40 cWhite text-shadow" aria-hidden="true"></i><br>
+						<span class="cWhite text-shadow s18 text-600">Que hacer?</span>	
+					</div>
+				</li>
+				<li class="places col-xs-6  bgBlue h120 block noPadding ">
+					<div class="content middle text-center">
+						<i class="fa fa-map-marker s40 cWhite text-shadow" aria-hidden="true"></i><br>
+						<span class="cWhite text-shadow s18 text-600">Lugares</span>	
+					</div>
+				</li>
+				<li class="wathDo col-xs-6  bgBlue h120 block noPadding ">
+					<div class="content middle text-center">
+						<i class="fa fa-list-ul s40 cWhite text-shadow" aria-hidden="true"></i><br>
+						<span class="cWhite text-shadow s18 text-600">Que hacer?</span>	
+					</div>
+				</li>
+				<li class="places col-xs-6  bgBlue h120 block noPadding ">
+					<div class="content middle text-center">
+						<i class="fa fa-map-marker s40 cWhite text-shadow" aria-hidden="true"></i><br>
+						<span class="cWhite text-shadow s18 text-600">Lugares</span>	
+					</div>
+				</li>
+				<li class="wathDo col-xs-6  bgBlue h120 block noPadding ">
+					<div class="content middle text-center">
+						<i class="fa fa-list-ul s40 cWhite text-shadow" aria-hidden="true"></i><br>
+						<span class="cWhite text-shadow s18 text-600">Que hacer?</span>	
+					</div>
+				</li>
+				<li class="places col-xs-6  bgBlue h120 block noPadding ">
+					<div class="content middle text-center">
+						<i class="fa fa-map-marker s40 cWhite text-shadow" aria-hidden="true"></i><br>
+						<span class="cWhite text-shadow s18 text-600">Lugares</span>	
+					</div>
+				</li>
+		
+
+			</ul>
+		</div>
+	</body>
+	<footer>
+		
+	</footer>
+</body>
+</html>
