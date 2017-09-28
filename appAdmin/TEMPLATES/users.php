efd0321e5b4c1ecca9924680b0e3df8fe0c1994e
carlosmorin <carlosmorin78@gmail.com>
2017-09-27 21:56:01 -0500

    ajustes

--- appAdmin/TEMPLATES/users.php
+++ appAdmin/TEMPLATES/users.php
@@ -1,6 +1,13 @@
 <div class="containerC">
 
 	<h1 class="titlePanel">Usuarios	</h1>
+	<div class="row">	
+		<div class="col-lg-2">
+		<button class="bgGreen buttonOrange">Nuevo usuario</button>	
+		</div>
+	</div>
+	
+	<div class="clear">	</div>
 	<div class="tableContainer">
 		<table>
 			<thead>
@@ -13,7 +20,50 @@
 					<th>EDIT</th>
 				</tr>
 			</thead>
+			<tbody>
+				<tr>
+					<th>lorem</th>
+					<th>lorem</th>
+					<th>lorem</th>
+					<th>lorem</th>
+					<th>lorem</th>
+					<th>lorem</th>
+				</tr>
+				<tr>
+					<th>lorem</th>
+					<th>lorem</th>
+					<th>lorem</th>
+					<th>lorem</th>
+					<th>lorem</th>
+					<th>lorem</th>
+				</tr>
+				<tr>
+					<th>lorem</th>
+					<th>lorem</th>
+					<th>lorem</th>
+					<th>lorem</th>
+					<th>lorem</th>
+					<th>lorem</th>
+				</tr>
+				<tr>
+					<th>lorem</th>
+					<th>lorem</th>
+					<th>lorem</th>
+					<th>lorem</th>
+					<th>lorem</th>
+					<th>lorem</th>
+				</tr>
+				<tr>
+					<th>lorem</th>
+					<th>lorem</th>
+					<th>lorem</th>
+					<th>lorem</th>
+					<th>lorem</th>
+					<th>lorem</th>
+				</tr>
+			</tbody>
 
 		</table>
 	</div>
 </div>
+<div class="bgBlack">	</div>
